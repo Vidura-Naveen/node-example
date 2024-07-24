cc8fd989ccc02912ba025795a735d83d62e554d5
Vidura-Naveen <viduranaween@gmail.com>
2024-07-24 13:05:48 +0530

    1

---
+++ index.js
@@ -0,0 +1,8 @@
+//npm init
+//node index
+
+// console.log("hellow world");
+
+const {generateRandomNumber,celsiusToFahranight} = require('./utill');
+console.log(`RandomNumber : ${generateRandomNumber()}`)
+console.log(`C To F : ${celsiusToFahranight(50)}`)
