6f8d6526e72e2db50c75bb34ebe74469431e5596
Vidura-Naveen <viduranaween@gmail.com>
2024-07-24 13:22:04 +0530

    2

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 
 // console.log("hellow world");
 
-const {generateRandomNumber,celsiusToFahranight} = require('./utill');
-console.log(`RandomNumber : ${generateRandomNumber()}`)
-console.log(`C To F : ${celsiusToFahranight(50)}`)
+// const {generateRandomNumber,celsiusToFahranight} = require('./utill');
+// console.log(`RandomNumber : ${generateRandomNumber()}`);
+// console.log(`C To F : ${celsiusToFahranight(50)}`);
+
+// import { getPosts } from "./postController.js"; // package.json eke "type": "module", kiyala demmain passe mehema import karanna puluwan nettam uda widiyata manually
+// console.log(getPosts()); // postController eke default netuwa 
+
+import  getPosts , {getPostLength}  from "./postController.js"; // package.json eke "type": "module", kiyala demmain passe mehema import karanna puluwan nettam uda widiyata manually
+console.log(getPosts()); // postController eke default demmama import {getPosts} kiyala warahan one ne
+console.log(` Post Length : ${getPostLength()}`);
---
+++ postController.js
@@ -0,0 +1,16 @@
+// const posts =[
+//     {id:1 ,title:"Post 1"},
+//     {id:2 ,title:"Post 2"},
+// ]
+
+// const getPosts = () => posts;
+// export {getPosts};
+
+const posts =[
+    {id:1 ,title:"Post 1"},
+    {id:2 ,title:"Post 2"},
+]
+
+const getPosts = () => posts;
+export const getPostLength = () => posts.length;
+export default getPosts;
